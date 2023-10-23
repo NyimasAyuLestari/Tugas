794d5da50b63a158f98e247a6cb59c478b59d184
NyimasAyuLestari <nyimasayulestari444@gmail.com>
2023-10-23 14:13:06 +0700

    commit tugas 1

---
+++ latihan.php
@@ -0,0 +1,14 @@
+<?php
+
+$nim = "701220315";
+$nama = "Nyimas Ayu Lestari";
+$umur = 19;
+$nilai = 75.75;
+
+echo "nim   : ". $nim . "</br>";
+echo "nama  : $nama</br>";
+print "umur : ". $umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digit dibelakang koma
+printf ("nilai : %.3f</br>", $nilai);
+
+?>
